0504dd24eefdf0edce8f8ebabb522054a2438016
EduardoJablinski <rm550975@fiap.com.br>
2023-11-13 04:38:10 -0300

    Ajustes

--- src/main/java/br/com/portoseguro/andromeda/controller/UsuarioController.java
+++ src/main/java/br/com/portoseguro/andromeda/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package br.com.portoseguro.andromeda.controller;
 
+import br.com.portoseguro.andromeda.dominio.Usuario;
 import br.com.portoseguro.andromeda.dominio.Veiculo;
 import br.com.portoseguro.andromeda.infra.dao.UsuarioDAO;
 import br.com.portoseguro.andromeda.infra.dao.VeiculoDAO;
@@ -47,6 +48,35 @@ public class UsuarioController {
                     .build();
         }
     }
+    
+    @POST
+    @Path("/cadastrar")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response cadastrarUsuario(UsuarioRequest usuarioRequest) {
+        try {
+            UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+            Usuario novoUsuario = new Usuario(
+                usuarioRequest.getNome(),
+                usuarioRequest.getEndereco(),
+                usuarioRequest.getCpfCnpj(),
+                usuarioRequest.getEmail(),
+                usuarioRequest.getTelefone()
+            );
+
+            usuarioDAO.adicionar(novoUsuario);
+
+            return Response.status(Response.Status.CREATED)
+                    .entity("Usuário cadastrado com sucesso")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Erro ao cadastrar o usuário")
+                    .build();
+        }
+    }
+
 
     public static class VeiculoResponse {
         private String placaVeiculo;
@@ -67,6 +97,30 @@ public class UsuarioController {
 
         public void setNumeroApolice(Long numeroApolice) {
             this.numeroApolice = numeroApolice;
+        }
+    }
+    
+    public static class UsuarioRequest {
+        private String nome;
+        private String endereco;
+        private String cpfCnpj;
+        private String email;
+        private String telefone;
+        
+        public String getNome() {
+            return nome;
+        }
+        public String getEndereco() {
+            return endereco;
+        }
+        public String getCpfCnpj() {
+            return cpfCnpj;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public String getTelefone() {
+            return telefone;
         }
     }
 
